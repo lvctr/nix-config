ed6fc39339761c1f21e7d3625ffb5fc232769dcd
alvi kazi <alvi@kazi.sh>
2026-09-06 22:29:19 +0900

    fix google sans again so its literally the git repo

--- pkgs/ttf-google-sans.nix
+++ pkgs/ttf-google-sans.nix
@@ -1,4 +1,4 @@
-{ stdenvNoCC, fetchurl }:
+{ stdenvNoCC, fetchgit }:
 let
   rev = "43590e625ab1b07f6a5809287ce16f7e61d9e165";
 in
@@ -6,17 +6,17 @@ stdenvNoCC.mkDerivation {
   pname = "ttf-google-sans";
   version = "unstable-2026-09-06";
 
-  src = fetchurl {
-    url = "https://flutter.googlesource.com/gallery-assets/+archive/${rev}/lib/fonts.tar.gz";
-    hash = "sha256-7uttbYqcMHcZ92kBHhOKZ1wDmaByZT7Ngfv0eTpBC84=";
+  src = fetchgit {
+    url = "https://flutter.googlesource.com/gallery-assets";
+    inherit rev;
+    hash = "sha256-/l1dYwkLlYIDnRrVZYXHeo1aFTMj4DQMV2Jkj7EgxXc=";
   };
 
-  sourceRoot = ".";
   dontBuild = true;
   installPhase = ''
     runHook preInstall
 
-    install -Dm644 GoogleSans*.ttf -t $out/share/fonts/truetype/google-sans
+    install -Dm644 lib/fonts/GoogleSans*.ttf -t $out/share/fonts/truetype/google-sans
 
     runHook postInstall
   '';

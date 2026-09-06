877bcfa1a0c97575cbbb33b617d2b641f1d29c91
alvi kazi <alvi@kazi.sh>
2026-09-07 00:37:08 +0900

    fix rofi

--- home/common/session.nix
+++ home/common/session.nix
@@ -23,8 +23,6 @@ in
     "hypr/hyprpaper.conf" = mkConfigSource (hyprConfigDir + /hyprpaper.conf);
     "hypr/xdph.conf" = mkConfigSource (hyprConfigDir + /xdph.conf);
 
-    "rofi/config.rasi" = mkConfigSource ./config/rofi/config.rasi;
-
     "dunst/dunstrc" = mkConfigSource ./config/dunst/dunstrc;
     "rofi/dunst.rasi" = mkConfigSource ./config/rofi/dunst.rasi;
 
